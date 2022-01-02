4db99a95eb1cb3449a48333c9016b947822a52f2
skrao <rao.saikrishna@gmail.com>
2022-01-01 22:44:01 -0600

    Added CORS Config

--- src/main/java/org/saikrishna/apps/mynotes/MyNotesApplication.java
+++ src/main/java/org/saikrishna/apps/mynotes/MyNotesApplication.java
@@ -2,9 +2,14 @@ package org.saikrishna.apps.mynotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyNotesApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(MyNotesApplication.class, args);
---
+++ src/main/java/org/saikrishna/apps/mynotes/config/CORSConfig.java
@@ -0,0 +1,25 @@
+package org.saikrishna.apps.mynotes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class CORSConfig {
+    @Bean
+    WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost*")
+                        .allowCredentials(false)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
